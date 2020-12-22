1c66e6f47f82cbcd5a75378cbc38f7c18bf032ad
Eduardo Souza <edu_costaaraujo@hotmail.com>
2020-12-22 19:17:29 -0300

    Added max bounds to the map

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { MapContainer, ImageOverlay } from 'react-leaflet';
+import { MapContainer, ImageOverlay, Marker } from 'react-leaflet';
 import {CRS} from 'leaflet';
 import gtasamap from './assets/maps/gtasamap.jpg'
 import './App.css';
@@ -7,9 +7,14 @@ function App() {
   return (
     <div className="App">
       <MapContainer center={[1500, 1500]} 
-                    zoom={0} minZoom={-3} maxZoom={2} 
-                    crs={CRS.Simple}>
+                    zoom={-2} minZoom={-3} maxZoom={2} 
+                    crs={CRS.Simple}
+                    maxBounds={[[-50, -50], [3122, 3122]]}
+                    maxBoundsViscosity={0.5}
+                    doubleClickZoom={false}>
         <ImageOverlay url={gtasamap} bounds={[[0, 0], [3072, 3072]]}/>
+        <Marker position={[1500, 1800]}></Marker>
+        <Marker position={[1100, 1500]}></Marker>
       </MapContainer>
     </div>
   );
