c77762ea8ae09a5193e225427e0f1ac9b30faf0b
Eduardo Souza <edu_costaaraujo@hotmail.com>
2020-12-23 22:34:41 -0300

    Markers added

--- src/App.js
+++ src/App.js
@@ -1,20 +1,25 @@
-import { MapContainer, ImageOverlay, Marker } from 'react-leaflet';
-import {CRS} from 'leaflet';
-import gtasamap from './assets/maps/gtasamap.jpg'
+import { MapContainer, ImageOverlay, Marker, Popup } from 'react-leaflet';
+import { CRS } from 'leaflet';
+import gtasamap from './assets/maps/gtasamap.jpg';
+import { oystersLocations } from './data/oysters';
 import './App.css';
 
 function App() {
+
   return (
     <div className="App">
       <MapContainer center={[1500, 1500]} 
                     zoom={-2} minZoom={-3} maxZoom={2} 
                     crs={CRS.Simple}
-                    maxBounds={[[-50, -50], [3122, 3122]]}
+                    maxBounds={[[-500, -500], [3650, 3650]]}
                     maxBoundsViscosity={0.5}
                     doubleClickZoom={false}>
-        <ImageOverlay url={gtasamap} bounds={[[0, 0], [3072, 3072]]}/>
-        <Marker position={[1500, 1800]}></Marker>
-        <Marker position={[1100, 1500]}></Marker>
+        <ImageOverlay url={gtasamap} bounds={[[0, 0], [3150, 3150]]}/>
+        {oystersLocations.map((oyster) => {
+          return (
+            <Marker position={[oyster.lat, oyster.lng]} key={oyster.id}></Marker>
+          );
+        })}
       </MapContainer>
     </div>
   );
---
+++ src/data/oysters.js
@@ -0,0 +1,303 @@
+export const oystersLocations = [
+    {
+      "id": 1,
+      "lat" : 520,
+      "lng" : 3096,
+      "url": "placeholder"
+    },
+    {
+      "id": 2,
+      "lat" : 254,
+      "lng" : 2973,
+      "url": "placeholder"
+    },
+    {
+      "id": 3,
+      "lat" : 280,
+      "lng" : 2912,
+      "url": "placeholder"
+    },
+    {
+      "id": 4,
+      "lat" : 210,
+      "lng" : 2768,
+      "url": "placeholder"
+    },
+    {
+      "id": 5,
+      "lat" : 160,
+      "lng" : 2260,
+      "url": "placeholder"
+    },
+    {
+      "id": 6,
+      "lat" : 446,
+      "lng" : 2070,
+      "url": "placeholder"
+    },
+    {
+      "id": 7,
+      "lat" : 630,
+      "lng" : 1945,
+      "url": "placeholder"
+    },
+    {
+      "id": 8,
+      "lat" : 561,
+      "lng" : 1652,
+      "url": "placeholder"
+    },
+    {
+      "id": 9,
+      "lat" : 758,
+      "lng" : 1948,
+      "url": "placeholder"
+    },
+    {
+      "id": 10,
+      "lat" : 958,
+      "lng" : 2582,
+      "url": "placeholder"
+    },
+    {
+      "id": 11,
+      "lat" : 1160,
+      "lng" : 2226,
+      "url": "placeholder"
+    },
+    {
+      "id": 12,
+      "lat" : 1512,
+      "lng" : 2642,
+      "url": "placeholder"
+    },
+    {
+      "id": 13,
+      "lat" : 1697,
+      "lng" : 2688,
+      "url": "placeholder"
+    },
+    {
+      "id": 14,
+      "lat" : 1827,
+      "lng" : 2990,
+      "url": "placeholder"
+    },
+    {
+      "id": 15,
+      "lat" : 1445,
+      "lng" : 1825,
+      "url": "placeholder"
+    },
+    {
+      "id": 16,
+      "lat" : 1300,
+      "lng" : 1596,
+      "url": "placeholder"
+    },
+    {
+      "id": 17,
+      "lat" : 1110,
+      "lng" : 1536,
+      "url": "placeholder"
+    },
+    {
+      "id": 18,
+      "lat" : 1054,
+      "lng" : 1608,
+      "url": "placeholder"
+    },
+    {
+      "id": 19,
+      "lat" : 905,
+      "lng" : 1597,
+      "url": "placeholder"
+    },
+    {
+      "id": 20,
+      "lat" : 720,
+      "lng" : 1481,
+      "url": "placeholder"
+    },
+    {
+      "id": 21,
+      "lat" : 111,
+      "lng" : 1007,
+      "url": "placeholder"
+    },
+    {
+      "id": 22,
+      "lat" : 224,
+      "lng" : 967,
+      "url": "placeholder"
+    },
+    {
+      "id": 23,
+      "lat" : 740,
+      "lng" : 725,
+      "url": "placeholder"
+    },
+    {
+      "id": 24,
+      "lat" : 1030,
+      "lng" : 97,
+      "url": "placeholder"
+    },
+    {
+      "id": 25,
+      "lat" : 1341,
+      "lng" : 182,
+      "url": "placeholder"
+    },
+    {
+      "id": 26,
+      "lat" : 1572,
+      "lng" : 743,
+      "url": "placeholder"
+    },
+    {
+      "id": 27,
+      "lat" : 1820,
+      "lng" : 1060,
+      "url": "placeholder"
+    },
+    {
+      "id": 28,
+      "lat" : 1690,
+      "lng" : 1170,
+      "url": "placeholder"
+    },
+    {
+      "id": 29,
+      "lat" : 1772,
+      "lng" : 882,
+      "url": "placeholder"
+    },
+    {
+      "id": 30,
+      "lat" : 1825,
+      "lng" : 942,
+      "url": "placeholder"
+    },
+    {
+      "id": 31,
+      "lat" : 2069,
+      "lng" : 932,
+      "url": "placeholder"
+    },
+    {
+      "id": 32,
+      "lat" : 2336,
+      "lng" : 815,
+      "url": "placeholder"
+    },
+    {
+      "id": 33,
+      "lat" : 2364,
+      "lng" : 278,
+      "url": "placeholder"
+    },
+    {
+      "id": 34,
+      "lat" : 2380,
+      "lng" : 202,
+      "url": "placeholder"
+    },
+    {
+      "id": 35,
+      "lat" : 2684,
+      "lng" : 204,
+      "url": "placeholder"
+    },
+    {
+      "id": 36,
+      "lat" : 2764,
+      "lng" : 487,
+      "url": "placeholder"
+    },
+    {
+      "id": 37,
+      "lat" : 2444,
+      "lng" : 791,
+      "url": "placeholder"
+    },
+    {
+      "id": 38,
+      "lat" : 2045,
+      "lng" : 1149,
+      "url": "placeholder"
+    },
+    {
+      "id": 39,
+      "lat" : 2019,
+      "lng" : 1245,
+      "url": "placeholder"
+    },
+    {
+      "id": 40,
+      "lat" : 2275,
+      "lng" : 1151,
+      "url": "placeholder"
+    },
+    {
+      "id": 41,
+      "lat" : 2663,
+      "lng" : 1177,
+      "url": "placeholder"
+    },
+    {
+      "id": 42,
+      "lat" : 2695,
+      "lng" : 1031,
+      "url": "placeholder"
+    },
+    {
+      "id": 43,
+      "lat" : 2917,
+      "lng" : 1091,
+      "url": "placeholder"
+    },
+    {
+      "id": 44,
+      "lat" : 3072,
+      "lng" : 1972,
+      "url": "placeholder"
+    },
+    {
+      "id": 45,
+      "lat" : 2552,
+      "lng" : 2652,
+      "url": "placeholder"
+    },
+    {
+      "id": 46,
+      "lat" : 2430,
+      "lng" : 2604,
+      "url": "placeholder"
+    },
+    {
+      "id": 47,
+      "lat" : 2374,
+      "lng" : 2874,
+      "url": "placeholder"
+    },
+    {
+      "id": 48,
+      "lat" : 2165,
+      "lng" : 2670,
+      "url": "placeholder"
+    },
+    {
+      "id": 49,
+      "lat" : 2791,
+      "lng" : 2892,
+      "url": "placeholder"
+    },
+    {
+      "id": 50,
+      "lat" : 3116,
+      "lng" : 3095,
+      "url": "placeholder"
+    },
+
+];
